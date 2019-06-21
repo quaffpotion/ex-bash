f7b0292ad9d9150227091836578fa4d16b29fe6b
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-06-21 18:18:41 -0500

    bash specific comments

--- git-add-all-commit-all.sh
+++ git-add-all-commit-all.sh
@@ -1,5 +1,7 @@
 fn ggc() {
+	#Here $# holds number of arguments passed to function
 	if [ $# = 0 ] ; then
+		#Here $(tput setab 1) expands to a color code
 		echo "$(tput setab 1)$(tput setaf 7)\nNo commit message received" 
 		return 1
 	fi
@@ -11,18 +13,20 @@ fn ggc() {
 	git status
 	pp "\ncommitting with message $1..."
 	git commit -m $1
+	#Here $? contains the exit status of the last commmand; 0 is success anything else is failure
 	if [ $? = 0 ] ; then
-		echo "$(tput setab 2)$(tput setaf 0)\nSuccessfully committed changes$sdc"
+		#Here $(tput sgr0) expands to default color codes
+		echo "$(tput setab 2)$(tput setaf 0)\nSuccessfully committed changes$(tput sgr0)"
 		pp "\npushing commit..."
 		if git push origin ; then
-			echo "$(tput setab 2)$(tput setaf 0)\nSuccessfully pushed changes$sdc"
+			echo "$(tput setab 2)$(tput setaf 0)\nSuccessfully pushed changes$(tput sgr0)"
 			return 0
 		else
-			echo "$(tput setab 1)$(tput setaf 7)\nFailed to push changes$sdc" 
+			echo "$(tput setab 1)$(tput setaf 7)\nFailed to push changes$(tput sgr0)" 
 			return 1
 		fi
 	else
-	    echo "$(tput setab 1)$(tput setaf 7)\nFailed to commit changes$sdc"
+	    echo "$(tput setab 1)$(tput setaf 7)\nFailed to commit changes$(tput sgr0)"
 	    return 1
 	fi
 }
